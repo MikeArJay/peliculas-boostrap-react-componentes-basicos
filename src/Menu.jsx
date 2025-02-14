52b1347448c7b9473f88e8947542470094e8f880
MikeArJay <michael.jam13@gmail.com>
2025-02-14 16:37:51 +0100

    menu hamburguesa oscuro y colapsa

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -3,7 +3,7 @@ import Container from "react-bootstrap/esm/Container";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import {Link} from 'react-router';
+import { Link } from 'react-router';
 
 
 export const Menu = ({ categorias, directores }) => {
@@ -11,22 +11,24 @@ export const Menu = ({ categorias, directores }) => {
 
 
     return (
-        <Navbar bg="light" data-bs-theme="light" sticky="top">
+        <Navbar bg="light" expand='md' data-bs-theme="light" sticky="top" className="justify-content-start">
             <Container>
-                <Link  to='/'><Navbar.Brand>Peliculas</Navbar.Brand></Link>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" >
+                <Navbar.Brand as={Link} to={'/'}>Peliculas</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" bg="light" className="me-auto bg-dark"/>
+                <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <NavDropdown title="Categorías" id="categorias">
-                            {categorias && categorias.map( (categoria, index) =>
-                                <NavDropdown.Item key={index}><Link to={`/${categoria}`}>{categoria}</Link></NavDropdown.Item>
-                            )
+                            {categorias
+                                && categorias.map((categoria, index) =>
+                                    <NavDropdown.Item key={index} as={Link} to={`/${categoria}`}>{categoria}</NavDropdown.Item>
+                                )
                             }
                         </NavDropdown>
                         <NavDropdown title="Directores" id="directores">
-                            {directores && directores.map( (director, index) =>
-                                <NavDropdown.Item key={index}><Link to={`/${director}`}>{director}</Link></NavDropdown.Item>
-                            )
+                            {directores
+                                && directores.map((director, index) =>
+                                    <NavDropdown.Item key={index} as={Link} to={`/${director}`}>{director}</NavDropdown.Item>
+                                )
                             }
                         </NavDropdown>
                     </Nav>
